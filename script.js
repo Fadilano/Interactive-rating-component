ec95a9dc5fa0060d581bdb447644283b9417c208
Fadilano <fadilanoa@gmail.com>
2023-08-21 20:46:41 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitBtn");
+  const result = document.getElementById("result");
+
+  submitBtn.addEventListener("click", function () {
+    const ratingSelect = document.querySelector('input[name="rating"]:checked');
+
+    if (ratingSelect) {
+      const selectedRating = ratingSelect.value;
+      result.textContent = `You selected ${selectedRating} out of 5`;
+    } else {
+      result.textContent = "none";
+    }
+    const ratingContainer = document.querySelector(".container-1");
+    const thankYouContainer = document.querySelector(".container-2");
+    ratingContainer.style.display = "none";
+    thankYouContainer.style.display = "flex";
+  });
+});

8bf56be547b73a7260411ebb6a435ec98d94f852
Fadilano <100310775+Fadilano@users.noreply.github.com>
2023-08-21 21:30:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const selectedRating = ratingSelect.value;
       result.textContent = `You selected ${selectedRating} out of 5`;
     } else {
-      result.textContent = "You selected X out of 5"";
+      result.textContent = "You selected X out of 5";
     }
     const ratingContainer = document.querySelector(".container-1");
     const thankYouContainer = document.querySelector(".container-2");
